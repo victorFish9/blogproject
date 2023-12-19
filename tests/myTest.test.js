2674a0701041b52c31ae194e78813e7a6387dd7b
Victor <aue228lublupacanov@gmail.com>
2023-12-19 20:33:15 +0200

    added tests for return highest value

--- tests/myTest.test.js
+++ tests/myTest.test.js
@@ -24,3 +24,26 @@ describe('total likes', () => {
         expect(result).toBe(5)
     })
 })
+describe('favorite blog', () => {
+    const listWithBlogs = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            likes: 5,
+        },
+        {
+            title: "Canonical string reduction",
+            author: "Edsger W. Dijkstra",
+            likes: 12
+        }
+    ]
+
+    test('show favorite blog with most liked post', () => {
+        const result = listHelper.favoriteBlog(listWithBlogs)
+        /*
+        expect(result.likes).toEqual(12)*/
+        const maxLikes = Math.max(...listWithBlogs.map(b => b.likes))
+        expect(result.likes).toEqual(maxLikes)
+    })
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,17 @@ const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    const mostLikedBlog = blogs.reduce((prev, current) => (prev.likes > current.likes ? prev : current))
+    return {
+        title: mostLikedBlog.title,
+        author: mostLikedBlog.author,
+        likes: mostLikedBlog.likes,
+    }
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
